290db967f3c39caa11b417d373a1904928bb488e
JemDEV-S <jdryshq1234@gmail.com>
2025-12-04 21:25:01 -0500

    cambios para las rutas api que se llamaan en el create

--- Modules/JobPosting/resources/views/create.blade.php
+++ Modules/JobPosting/resources/views/create.blade.php
@@ -614,7 +614,7 @@ $(document).ready(function() {
         $('#code-text').text('Generando...');
 
         // Llamar a la API
-        $.get('/api/generate/job-posting-code', { year: year })
+        $.get('{{ route('generate.job-posting-code') }}', { year: year })
             .done(function(response) {
                 if (response.success) {
                     $('#code-text').text(response.code);
@@ -704,7 +704,7 @@ $(document).ready(function() {
         `);
 
         // Llamar a la API
-        $.post('/api/preview/schedule', {
+        $.post('{{ route('preview.schedule') }}', {
             start_date: startDate,
             _token: csrfToken
         })
