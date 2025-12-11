11e8047ef031750c88ec5aaefc1b043ca083bdd6
JemDEV-S <jdryshq1234@gmail.com>
2025-12-11 09:36:43 -0500

    composer dump 3

---
+++ composer-run.php
@@ -0,0 +1,18 @@
+<?php
+// composer-run.php
+echo "<pre>";
+echo "Ejecutando composer dump-autoload...\n\n";
+
+// Cambiar al directorio del proyecto
+chdir(__DIR__);
+
+// Ejecutar composer dump-autoload
+$output = shell_exec('composer dump-autoload 2>&1');
+echo $output;
+
+echo "\n\nProceso completado.";
+echo "</pre>";
+
+// Opcional: Auto-eliminar este archivo después de ejecutarlo
+// unlink(__FILE__);
+?>
