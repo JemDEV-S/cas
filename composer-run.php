e2346310458cbdefbd5f6b46d373d2f73e673bd3
JemDEV-S <jdryshq1234@gmail.com>
2025-12-11 09:42:38 -0500

    composer dump 6

--- composer-run.php
+++ composer-run.php
@@ -3,43 +3,16 @@
 echo "<pre>";
 echo "Ejecutando composer dump-autoload...\n\n";
 
+// Cambiar al directorio del proyecto
 chdir(__DIR__);
 
-// Intentar diferentes rutas de composer
-$composerPaths = [
-    'composer.phar',
-    'composer',
-    '/usr/local/bin/composer',
-    '/usr/bin/composer',
-    'php composer.phar',
-    '/usr/local/bin/php composer.phar'
-];
-
-$executed = false;
-
-foreach ($composerPaths as $composer) {
-    echo "Intentando con: $composer\n";
-    $output = shell_exec("$composer dump-autoload 2>&1");
-    
-    if ($output && strpos($output, 'command not found') === false) {
-        echo $output;
-        $executed = true;
-        break;
-    }
-}
-
-if (!$executed) {
-    echo "No se encontró composer. Intentando solución alternativa...\n\n";
-    
-    // Si composer.phar existe, usarlo con PHP
-    if (file_exists('composer.phar')) {
-        $output = shell_exec('php composer.phar dump-autoload 2>&1');
-        echo $output;
-    } else {
-        echo "composer.phar no encontrado. Necesitamos descargarlo o usar solución manual.\n";
-    }
-}
+// Ejecutar composer dump-autoload
+$output = shell_exec('composer2 dump-autoload 2>&1');
+echo $output;
 
 echo "\n\nProceso completado.";
 echo "</pre>";
+
+// Opcional: Auto-eliminar este archivo después de ejecutarlo
+// unlink(__FILE__);
 ?>
